367e812768e7c230fbe4efb2654107ce827090cb
Nicola <lanzoni.nicola@gmail.com>
2022-07-06 12:08:39 -0300

    Trying configure aliases

--- frontend/templates-editor/webpack.config.js
+++ frontend/templates-editor/webpack.config.js
@@ -1,8 +1,8 @@
 //./node_modules/@fontsource/inter/500.css
-
 // TODO: ContextReplacementPlugin // date-fns project https://date-fns.org/v2.28.0/docs/webpack
 
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const BundleAnalyzerPlugin =
 //   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
@@ -14,8 +14,20 @@ const config = {
   mode: "production",
   resolve: {
     extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+    // alias: {
+    //   "countdown-provider": path.resolve(
+    //     __dirname,
+    //     "src",
+    //     "countdown-provider"
+    //   ),
+    // },
   },
-  // plugins: [new BundleAnalyzerPlugin()],
+  plugins: [
+    // new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "index.css",
+    }),
+  ],
   module: {
     rules: [
       {
@@ -24,9 +36,13 @@ const config = {
         exclude: /node_modules/,
       },
       // loading font from @fontsource npm package
+      // {
+      //   test: /\.(scss|css)$/i,
+      //   use: [MiniCssExtractPlugin.loader, "style-loader", "css-loader"],
+      // },
       {
-        test: /\.(scss|css)$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       // load svg files
       {
