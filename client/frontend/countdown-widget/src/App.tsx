cedcd1e3ad71bf5540887b65b8d1b6ab93a19b77
Nicola <lanzoni.nicola@gmail.com>
2022-08-04 18:54:13 -0300

    chore: remove import not used

--- client/frontend/countdown-widget/src/App.tsx
+++ client/frontend/countdown-widget/src/App.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 import TimerSkeleton from "./countdown-widget/components/timer-skeleton/timer-skeleton";
 import CountdownWidget from "./countdown-widget/countdown-widget";
 import useEditorSettings from "./countdown-widget/hooks/useEditorSettings";
