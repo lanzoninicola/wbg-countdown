e669ee9deb64052006288dcd9a7cf1f06e380ccd
Nicola <lanzoni.nicola@gmail.com>
2022-08-09 14:39:14 -0300

    feat(onboarding-fe): move config inside the OnboardingProvider

--- client/frontend/templates-editor/src/App.tsx
+++ client/frontend/templates-editor/src/App.tsx
@@ -7,24 +7,10 @@ import MainContent from "./global/common/layout/main-content/main-content";
 import EditorPage from "./editor/editor-page";
 import { OnboardingProvider } from "./onboarding";
 
-/** START: Info localized by Wordpress */
-// @ts-ignore
-const productId = "1"; //clockdownLocalized.product_id;
-// @ts-ignore
-const installationId = "7cg9997b-0f40-11ed-9cce-040e3caabadb";
-//clockdownLocalized.installation_id;
-// @ts-ignore
-const onboardingStatus = "pending"; //clockdownLocalized.onboarding_status;
-/** END: Info localized by Wordpress */
-
 function App() {
   return (
     <MainContent>
-      <OnboardingProvider
-        currentStatus={onboardingStatus}
-        productId={productId}
-        installationId={installationId}
-      >
+      <OnboardingProvider>
         <Header />
       </OnboardingProvider>
       <EditorPage />
