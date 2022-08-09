2bb993d825cbe69735fa443f8847bfb4d760349a
Nicola <lanzoni.nicola@gmail.com>
2022-08-09 14:39:48 -0300

    feat(onboarding-form): create separated components

---
+++ client/frontend/templates-editor/src/onboarding/components/onboarding-modal-form/consent-group/consent-group.tsx
@@ -0,0 +1,43 @@
+import { VStack, Checkbox, Text } from "@chakra-ui/react";
+import { t } from "i18next";
+import { useTranslation } from "react-i18next";
+import FormRequiredIndicator from "../form-required-indicator/form-required-indicator";
+import { useOnboardingModalForm } from "../useOnboardingModalForm";
+import CheckboxLabel from "./checkbox-label/checkbox-label";
+
+export default function ConsentGroup() {
+  const { handleCheckboxChange } = useOnboardingModalForm();
+  const { t } = useTranslation();
+
+  return (
+    <VStack spacing={4} alignItems={"flex-start"}>
+      <Checkbox
+        size="md"
+        name="consent_newsletter"
+        onChange={handleCheckboxChange}
+      >
+        <CheckboxLabel>{t("onboarding.newsletterConsent")}</CheckboxLabel>
+      </Checkbox>
+      <Checkbox
+        size="md"
+        name="consent_terms"
+        onChange={handleCheckboxChange}
+        isRequired={true}
+      >
+        <CheckboxLabel>
+          <FormRequiredIndicator /> {t("onboarding.privacyConsent")}
+        </CheckboxLabel>
+      </Checkbox>
+      <Checkbox
+        size="md"
+        name="consent_privacy"
+        onChange={handleCheckboxChange}
+        isRequired={true}
+      >
+        <CheckboxLabel>
+          <FormRequiredIndicator /> {t("onboarding.termsAndConditionsConsent")}
+        </CheckboxLabel>
+      </Checkbox>
+    </VStack>
+  );
+}
