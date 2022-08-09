c61bd2d94e0bfa2899c2a315202064498f16b6c7
Nicola <lanzoni.nicola@gmail.com>
2022-08-09 14:41:04 -0300

    refactor(main-dev): little refactor createDevRoot

--- client/frontend/templates-editor/src/main.dev.tsx
+++ client/frontend/templates-editor/src/main.dev.tsx
@@ -1,10 +1,22 @@
 export default function createDevRoot() {
+  const cdata = {
+    apiURL: "http://localhost/bb-melhor-envio/wp-json",
+    language: "pt_BR",
+    onboarding_status: "pending",
+    product_id: "1",
+    installation_id: "7cg9997b-0f40-11ed-9cce-040e3caabadb",
+    nonce: "ec5a743f11",
+    wp_rest_nonce: "8f9c795c4e",
+  };
+
   const clockdownNode = document.createElement("div");
   clockdownNode.id = "clockdown";
   const scriptNode = document.createElement("script");
   scriptNode.type = "text/javascript";
   scriptNode.textContent = `
-    /* <![CDATA[ */var clockdownLocalized = {"apiURL":"http:\/\/localhost\/bb-melhor-envio\/wp-json","language":"pt_BR", "onboarding_status":"pending", "product_id": "1", "installation_id": "7cg9997b-0f40-11ed-9cce-040e3caabadb"};/* ]]&gt; */
+    /* <![CDATA[ */var clockdownLocalized = ${JSON.stringify(
+      cdata
+    )};/* ]]&gt; */
   `;
 
   // get body
