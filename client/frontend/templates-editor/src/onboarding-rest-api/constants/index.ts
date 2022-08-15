c17213e480d1795d9b7b23d09c86f88a397e3244
Nicola <lanzoni.nicola@gmail.com>
2022-08-15 10:45:28 -0300

    feat: update handler of CDATA

--- client/frontend/templates-editor/src/onboarding-rest-api/constants/index.ts
+++ client/frontend/templates-editor/src/onboarding-rest-api/constants/index.ts
@@ -1,6 +1,9 @@
-const REST_API_BASE_URL = "http://localhost/bb-melhor-envio/wp-json";
+import { getServerData } from "../../global/utils";
+
+const { apiURL: REST_API_BASE_URL, wp_rest_nonce: WP_REST_NONCE } =
+  getServerData();
 const REST_API_NAMESPACE_AND_VERSION = "/commerce/v1";
 
 const REST_API_URL = `${REST_API_BASE_URL}${REST_API_NAMESPACE_AND_VERSION}`;
 
-export { REST_API_URL };
+export { REST_API_URL, WP_REST_NONCE };
--- client/frontend/templates-editor/src/onboarding-rest-api/hooks/useOnboardingRestConfig.ts
+++ client/frontend/templates-editor/src/onboarding-rest-api/hooks/useOnboardingRestConfig.ts
@@ -1,12 +1,11 @@
-import { REST_API_URL } from "../constants";
+import { REST_API_URL, WP_REST_NONCE } from "../constants";
 import { WithEndpointEmptyParams, WithEndpointParamsId } from "../types";
 
 export default function useOnboardingRestConfig() {
   const disabledNonce = process.env.NODE_ENV === "development" && true;
   const headers = {
     "Content-Type": "application/json",
-    // @ts-ignore
-    "X-WP-Nonce": clockdownLocalized.wp_rest_nonce,
+    "X-WP-Nonce": WP_REST_NONCE,
   };
 
   if (disabledNonce) {
